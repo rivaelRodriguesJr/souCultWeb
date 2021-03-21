b14aa262bfa0b48630574ca684b9462f119de8ba
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2021-03-21 19:18:43 -0300

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
+import 'core/assets/styles/custom.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="text-success">Olá mundo!</h1>
+      <button type="button" className="btn btn-primary">Primary</button>
+    </>
+  )
 }
 
 export default App;
