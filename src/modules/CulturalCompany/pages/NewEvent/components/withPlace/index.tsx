d50b7e1257d7fbc8bffbfd4423a0b3e3328f821f
KelvinGomes <40004445+KelvinGomes@users.noreply.github.com>
2021-05-10 01:46:51 -0300

    Version 3

--- src/modules/CulturalCompany/pages/NewEvent/components/withPlace/index.tsx
+++ src/modules/CulturalCompany/pages/NewEvent/components/withPlace/index.tsx
@@ -14,72 +14,75 @@ class WithPlace extends React.Component {
         return (
             <>
                 <div className="container">
-                    <div className="container">
-                        <Form>
-                            <Form.Group as={Row} className="row">
-                                <Form.Label column sm="1">
-                                    Data
+                    <Form>
+                        <Form.Group as={Row} className="row">
+                            <Form.Label column sm="1">
+                                Data
                                 </Form.Label>
-                                <Col sm="2">
-                                    <Form.Control type="date" />
-                                </Col>
-
-                                <Form.Label column sm="1">
-                                    Horario
-                                </Form.Label>
-                                <Col sm="2">
-                                    <Form.Control type="time" />
-                                </Col>
-
-                                <Form.Label column sm="1">
-                                    Sala
-                                </Form.Label>
-                                <Col sm="2">
-                                    <Form.Control type="text" />
-                                </Col>
-
-                                <Form.Label column sm="1">
-                                    Plano:
-                                </Form.Label>
-                                <Col sm="2">
-                                    <Form.Control as="select">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </Form.Control>
-                                </Col>
-                            </Form.Group>
-                            <Table responsive className="table" >
-                                <thead className="tableHeader">
-                                    <tr>
-                                        <th>Área</th>
-                                        <th>Fileira</th>
-                                        <th>Qtd. assentos</th>
-                                        <th>
-                                            <Form.Check/>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {Array.from({ length: 3 }).map((_, index) => (
-                                        <tr>
-                                            {Array.from({ length: 3 }).map((_, index) => (
-                                                <td key={index}>Table cell {index}</td>
-                                            ))}
-                                            <td>
-                                                <Form.Check/>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </Table>
                             <Col sm="2">
-                                <Button className="button" variant="sea-blue-1">Adicionar sessão</Button>
+                                <Form.Control type="date" />
                             </Col>
-                        </Form>
-                    </div>
+
+                            <Form.Label column sm="1">
+                                Horario
+                                </Form.Label>
+                            <Col sm="2">
+                                <Form.Control type="time" />
+                            </Col>
+
+                            <Form.Label column sm="1">
+                                Sala
+                                </Form.Label>
+                            <Col sm="2">
+                                <Form.Control type="text" />
+                            </Col>
+
+                            <Form.Label column sm="1">
+                                Plano:
+                                </Form.Label>
+                            <Col sm="2">
+                                <Form.Control as="select">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Form.Control>
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Label column sm="3">
+                            Selecione os assentos disponíveis:
+                        </Form.Label>
+
+                        <Table responsive className="table" >
+                            <thead className="tableHeader">
+                                <tr>
+                                    <th>Área</th>
+                                    <th>Fileira</th>
+                                    <th>Qtd. assentos</th>
+                                    <th>
+                                        <Form.Check />
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Array.from({ length: 3 }).map((_, index) => (
+                                    <tr>
+                                        {Array.from({ length: 3 }).map((_, index) => (
+                                            <td key={index}>Table cell {index}</td>
+                                        ))}
+                                        <td>
+                                            <Form.Check />
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                        <Col sm="2">
+                            <Button className="button" variant="sea-blue-1">Adicionar sessão</Button>
+                        </Col>
+                    </Form>
                 </div>
             </>
 
