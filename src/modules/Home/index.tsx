910a8c000cc9ed5efd1221fbfb0d4e696f94381d
KelvinGomes <40004445+KelvinGomes@users.noreply.github.com>
2021-06-13 16:39:58 -0300

    Home V2

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,14 +1,10 @@
 import Navbar from '../../core/components/Navbar';
 import HomeRoutes from './HomeRoutes';
-import Banner from 'core/assets/images/banner.png';
-import React, { Card } from 'react-bootstrap';
-import './styles.scss';
 
 const Home = () => (
   <>
     <Navbar />
     <HomeRoutes />
-    <Card.Img className = "banner" variant="bottom" src={Banner} />
   </>
 );
 
--- src/modules/Home/pages/Home/index.tsx
+++ src/modules/Home/pages/Home/index.tsx
@@ -1,6 +1,21 @@
 
+import Banner from 'core/assets/images/banner.jpg';
+import React, { Card } from 'react-bootstrap';
+import './styles.scss';
+
 const Home = () => (
-  <></>
+  <>
+    <div className = "homeContainer">
+      <Card.Img className = "banner" variant="bottom" src={Banner} />
+      <Card>
+      <Card.Body>
+        <a href = "/home/register">
+          <h5>ENTRE EM CONTATO CONOSCO PARA FAZER PARTE DESSE GRANDE SHOW!</h5>
+        </a>
+      </Card.Body>
+      </Card>
+    </div>
+  </>
 );
 
 export default Home;
