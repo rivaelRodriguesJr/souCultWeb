e0bff54a2bc02b46a75e27313e805fcc3e775870
KelvinGomes <40004445+KelvinGomes@users.noreply.github.com>
2021-06-13 14:44:58 -0300

    Home V1

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,10 +1,14 @@
 import Navbar from '../../core/components/Navbar';
 import HomeRoutes from './HomeRoutes';
+import Banner from 'core/assets/images/banner.png';
+import React, { Card } from 'react-bootstrap';
+import './styles.scss';
 
 const Home = () => (
   <>
     <Navbar />
     <HomeRoutes />
+    <Card.Img className = "banner" variant="bottom" src={Banner} />
   </>
 );
 
