1eb2bc08734addc40ce60265e5543749fca7d94c
Rivael <rivael.rodriguesjr@outlook.com>
2021-11-17 20:33:48 -0300

    Ajuste na home

--- src/modules/Home/pages/Home/index.tsx
+++ src/modules/Home/pages/Home/index.tsx
@@ -1,18 +1,19 @@
 
 import Banner from 'core/assets/images/banner.jpg';
-import React, { Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Home = () => (
   <>
-    <div className = "homeContainer">
-      <Card.Img className = "banner" variant="bottom" src={Banner} />
+    <div className="homeContainer">
+      <Card.Img className="banner" variant="bottom" src={Banner} />
       <Card>
-      <Card.Body>
-        <a href = "/home/register">
-          <h5>ENTRE EM CONTATO CONOSCO PARA FAZER PARTE DESSE GRANDE SHOW!</h5>
-        </a>
-      </Card.Body>
+        <Card.Body>
+          <Link to="/home/register">
+            <h5>ENTRE EM CONTATO CONOSCO PARA FAZER PARTE DESSE GRANDE SHOW!</h5>
+          </Link>
+        </Card.Body>
       </Card>
     </div>
   </>
